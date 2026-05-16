4f4e4849608524ce2c808d7715608442a853fddd
shouichi33333 <shouichi20091213@gmail.com>
2026-05-16 16:20:27 +0900

    カメラ追尾

--- Assets/Input/PlayerInptActions.cs
+++ Assets/Input/PlayerInptActions.cs
@@ -109,6 +109,15 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""93733f15-0457-45b2-b8ae-48f0eb446567"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -177,6 +186,17 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
                     ""action"": ""Fire"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""db072fca-917c-408b-bfc0-a46a96e312d4"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -187,6 +207,7 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
     }
 
     ~@PlayerInptActions()
@@ -269,6 +290,7 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Look;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -288,6 +310,10 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Fire".
         /// </summary>
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Look".
+        /// </summary>
+        public InputAction @Look => m_Wrapper.m_Player_Look;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -320,6 +346,9 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
             @Fire.started += instance.OnFire;
             @Fire.performed += instance.OnFire;
             @Fire.canceled += instance.OnFire;
+            @Look.started += instance.OnLook;
+            @Look.performed += instance.OnLook;
+            @Look.canceled += instance.OnLook;
         }
 
         /// <summary>
@@ -337,6 +366,9 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
             @Fire.started -= instance.OnFire;
             @Fire.performed -= instance.OnFire;
             @Fire.canceled -= instance.OnFire;
+            @Look.started -= instance.OnLook;
+            @Look.performed -= instance.OnLook;
+            @Look.canceled -= instance.OnLook;
         }
 
         /// <summary>
@@ -391,5 +423,12 @@ public partial class @PlayerInptActions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnFire(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Look" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnLook(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/scripts/cameraController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+namespace jugyou.batoru.Camera
+{
+
+    public class cameraController : MonoBehaviour
+    {
+        private float lookSensitivity = 0.2f;
+
+        private float distance = 5;
+
+        private float heightOffset = 1.5f;
+
+        private float minPitch = -10;
+
+        private float maxPitch = 60;
+
+        [SerializeField] private Transform target;
+
+        private PlayerInptActions inputActions;
+
+        private Vector2 lookInput = Vector2.zero;
+
+        private float currentYaw = 0;
+
+        private float currentPitch = 20;
+
+        private void Awake()
+        {
+            inputActions = new PlayerInptActions();
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        private void OnEnable()
+        {
+            inputActions.Enable();
+        }
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+        private void Update()
+        {
+            lookInput = inputActions.Player.Look.ReadValue<Vector2>();
+
+            currentYaw += lookInput.x * lookSensitivity;
+            currentPitch -= lookInput.y * lookSensitivity;
+
+            currentPitch = Mathf.Clamp(currentPitch, minPitch, maxPitch);
+        }
+        private void LateUpdate()
+        {
+            if (target == null) return;
+            Vector3 targetPosi = target.position + Vector3.up * heightOffset;
+            Quaternion rotate = Quaternion.Euler(currentPitch, currentYaw, 0);
+
+            //注意点から、計算した角度から後ろ方向へ距離分だけ離した位置を計算
+            Vector3 cameraPosi = target.position - (rotate * Vector3.forward * distance);
+
+            transform.position = cameraPosi;
+            transform.rotation = rotate;
+        }
+    }
+}
