e94dcf24956b85c9cabd50a2861da3bd414494b3
shouichi33333 <shouichi20091213@gmail.com>
2026-05-16 14:48:15 +0900

    ネームスペース追加

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,66 +1,69 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace jugyou.batoru.Player
 {
-    private const float moveSpeed = 5f; //変更不可のスピード
-
-    [SerializeField] Rigidbody RB;  //リジットボディ
-
-    private PlayerInptActions inputActions;
-
-    private Vector2 moveInput = Vector2.zero;
-
-    private Vector3 moveDirection = Vector3.zero;   //移動方向
-    public Vector3 CurrentVelocity {  get; private set; }   //現在のベロシティを引き取れる
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        inputActions = new PlayerInptActions();
-        inputActions.Player.Fire.performed += Fire;
-    }
+        private const float moveSpeed = 5f; //変更不可のスピード
 
-    private void OnEnable()
-    {
-        inputActions.Enable();
-    }
-    private void OnDisable()
-    {
-        inputActions.Disable();
-    }
-    private void Update()
-    {
-        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
-    }
+        [SerializeField] Rigidbody RB;  //リジットボディ
 
-    private void FixedUpdate()
-    {
-        Move();
-    }
+        private PlayerInptActions inputActions;
 
-    private void Move()
-    {
-        if (RB == null)
+        private Vector2 moveInput = Vector2.zero;
+
+        private Vector3 moveDirection = Vector3.zero;   //移動方向
+        public Vector3 CurrentVelocity { get; private set; }   //現在のベロシティを引き取れる
+
+        private void Awake()
         {
-            Debug.LogError("リジットボディがついていないよ");
-            return;
-        }
-        if (moveInput == Vector2.zero)
-        {
-            RB.linearVelocity = new Vector3(0f, RB.linearVelocity.y, 0f);
-            CurrentVelocity = Vector3.zero;
-            return;
+            inputActions = new PlayerInptActions();
+            inputActions.Player.Fire.performed += Fire;
         }
 
-        Vector2 targetVelocity = new Vector2(moveInput.x, moveInput.y);
-        targetVelocity.Normalize();
-        Vector3 playerVelocity = new Vector3(targetVelocity.x, RB.linearVelocity.y, targetVelocity.y) * moveSpeed;
-        RB.linearVelocity = playerVelocity;
-        CurrentVelocity = RB.linearVelocity;
-    }
+        private void OnEnable()
+        {
+            inputActions.Enable();
+        }
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+        private void Update()
+        {
+            moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        }
 
-    private void Fire(InputAction.CallbackContext context)
-    {
-        Debug.Log("test");
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (RB == null)
+            {
+                Debug.LogError("リジットボディがついていないよ");
+                return;
+            }
+            if (moveInput == Vector2.zero)
+            {
+                RB.linearVelocity = new Vector3(0f, RB.linearVelocity.y, 0f);
+                CurrentVelocity = Vector3.zero;
+                return;
+            }
+
+            Vector2 targetVelocity = new Vector2(moveInput.x, moveInput.y);
+            targetVelocity.Normalize();
+            Vector3 playerVelocity = new Vector3(targetVelocity.x, RB.linearVelocity.y, targetVelocity.y) * moveSpeed;
+            RB.linearVelocity = playerVelocity;
+            CurrentVelocity = RB.linearVelocity;
+        }
+
+        private void Fire(InputAction.CallbackContext context)
+        {
+            Debug.Log("test");
+        }
     }
 }
